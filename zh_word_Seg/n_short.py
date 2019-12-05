d351bdc97d7f352a61509cbb42c0dab989c58062
rayx <ray_scqhg@163.com>
2019-12-05 16:13:24 +0800

    update CWS

--- zh_word_Seg/n_short.py
+++ zh_word_Seg/n_short.py
@@ -1,18 +1,6 @@
 # -*- coding:utf-8 -*-
 # @Time    : 2019/10/22 9:12
 # @Author  : Ray.X
-
-line_num = -1
-words = set()
-with open('../Data/news/C000008/10.txt', encoding='GBK') as f:
-    for line in f:
-        print(line)
-        line_num += 1
-        line = line.split()
-        if not line:
-            print(line)
-            continue
-        word_list = [i for i in line if i != '']
-        print(word_list)
-        words |= set(word_list)
-        print(words, '\n..........')
+"""
+    Use N-short for CWS
+"""
